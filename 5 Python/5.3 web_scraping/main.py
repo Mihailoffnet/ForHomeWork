743ee352dd0cbb2a00c9133149121ae4f5b5074f
Mihailoff <mihailoff@inbox.ru>
2023-07-11 22:38:24 +0500

    add 5.3

--- 5 Python/5.3 web_scraping/main.py
+++ 5 Python/5.3 web_scraping/main.py
@@ -1,94 +1,65 @@
-"""
-https://hh.ru/search/vacancy?area=1&area=2&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&text=description%3A%28python+AND+Flask+AND+Django%29
-"""
-# Маска очистки вакансии
-"""
-r"(.+)*\n*(.+)>(.+)</a>"
-r"\3"
-"""
-
-# Маска очистки зарплаты
-"""
-r"(.+)compensation\">(.+)(₽|$|€)+</span>(.+)"
-r"\2\3"
-"""
-"""
-Описание вакансии:
-<div class="vacancy-branded-user-content" itemprop="description" data-qa="vacancy-description"><p></div>
-"""
-
 import bs4
 import fake_headers
 import requests
 import re
 
-
 headers = fake_headers.Headers(browser="firefox", os="win")
 headers_dict = headers.generate()
 
 link = 'https://hh.ru/search/vacancy'
-search_text = 'python'
+search_text = 'description%3A%28python+AND+Flask+AND+Django%29'
 s_params_1, s_params_2 = 'area=1', 'area=2'
 search_period = 0 # поиск ваканский за последние Х дней (0 без ограничения)
 sort = 'publication_time' # сортировка
-page = 1
-target_link = f'{link}?text={search_text}&{s_params_1}&{s_params_2 }&page={page}&order_by={sort}&search_period={search_period}'
+page = 0
+target_link = f'{link}?{s_params_1}&{s_params_2 }&ored_clusters=true&text={search_text}&order_by={sort}&items_on_page=20'
 usd = False
-print(target_link)
-print()
 
-response = requests.get(url=target_link, headers=headers_dict)
-main_html_data = response.text
-main_html = bs4.BeautifulSoup(main_html_data, "lxml")
+def get_vacancies(url):
+    response = requests.get(url, headers=headers_dict)
+    main_html_data = response.text
+    main_html = bs4.BeautifulSoup(main_html_data, "lxml")
+    vacancies = main_html.find_all("div", class_="vacancy-serp-item__layout")
+    return vacancies
 
-
-jobs_list = main_html.find("div", id="a11y-main-content")
-vacancies = main_html.find_all("div", class_="vacancy-serp-item__layout")
+vacancies = get_vacancies(target_link)
 
 parsed_data = []
+while len(vacancies) != 0:
 
-for item in vacancies:
-    vacancy_url = item.find("a")['href']
-    salary_raw = item.find_all("span")
-    if 'vacancy-serp__vacancy-compensation' in str(salary_raw):
-        print()
-        print('вхождение')
-        print()
-        salary = re.sub(r"(.+)compensation\">(.+)(₽|$|€)+</span>(.+)", r"\3\4", str(salary_raw))
-    else: salary = None
-    company_name_raw = item.find("a")
-    company_name = re.sub(r"(.)*>(.+)</a>", r"\2", str(company_name_raw))
-    print(company_name)
-    print()
-    print(salary)
-    print()
-    print(vacancy_url)
-    print()
-    print()
-    # city_name = ''
+    for item in vacancies:
+        vacancy_url = item.find("a")['href']
+        if item.find('span', class_='bloko-header-section-3'):
+            salary = item.find('span', 
+                               class_='bloko-header-section-3').get_text()
+        else: 
+            salary = "Зарплата не указана"
 
+        company_name = item.find_all("div", class_='bloko-text')[0].get_text()
 
+        vacancy_name = item.find("a").get_text()
 
-    # a_tag = h2_tag.find("a")
-    # span_tag = a_tag.find("span")
-    # time_tag = article_tag.find("time")
+        city_raw = item.find_all("div", class_='bloko-text')[1].get_text()
+        city = re.sub(r"([a-яА-Я-]+)(\s|\.|\,)*(.)*", r"\1", city_raw)
+        # print(vacancy_name)
+        # print(company_name)
+        # print(salary)
+        # print(vacancy_url)
+        # print(city)
+        # print()
+        parsed_data.append(
+            {
+                "company_name": company_name,
+                "vacancy_name": vacancy_name,
+                "salary": salary,
+                "vacancy_url": vacancy_url,
+                "city": city,
+            }
+        )
+    
+    page += 1
+    vacancies = get_vacancies(f'{target_link}&page={page}')
 
-#     link = f"https://habr.com{a_tag['href']}"
-#     title = span_tag.text
-#     date_time = time_tag["datetime"]
+print('Скрапинг завершен')
+print(f'Найдено {len(parsed_data)} вакансий')
 
-#     response = requests.get(link, headers=headers.generate()).text
-#     article_html = bs4.BeautifulSoup(response, "lxml")
-#     article_full_tag = article_html.find("div", id="post-content-body")
-#     article_full_text = article_full_tag.text[100]
-
-#     parsed_data.append(
-#         {
-#             "title": title,
-#             "link": link,
-#             "date_time": date_time,
-#             "text": article_full_text,
-#         }
-#     )
-
-# print(parsed_data)
